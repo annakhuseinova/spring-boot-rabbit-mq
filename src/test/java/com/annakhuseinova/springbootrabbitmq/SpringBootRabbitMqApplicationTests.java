77a74a72be8d8c0f7da06d56d21925c68fe4e9bd
annakhuseinova <annakhuseinova@yandex.ru>
2020-07-14 14:39:22 +0300

    Initial commit

---
+++ src/test/java/com/annakhuseinova/springbootrabbitmq/SpringBootRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springbootrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
